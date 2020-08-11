ae95c244c4c36125f7a7bf922f48c76629c8d66d
angyeongeun <anruddms8124@gmail.com>
2020-08-11 16:20:32 +0900

    real-deploy test04

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,7 +28,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> jar name: $JAR_NAME"
 
-echo "> $JAR_NAME 네 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
@@ -37,3 +37,5 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \-Dspring.profiles.active=real \$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+vim nohup.out
